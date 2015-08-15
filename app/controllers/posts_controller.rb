f15206b7793e60220afdc71cbd8a42b50cdb6cb0
Anna Isabel Lamers <isabel.lamers@gmail.com>
2015-08-15 16:47:48 +0200

    Post CR and views

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,38 @@
+class PostsController < ApplicationController
+
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to post_path @post , notice: 'Post was created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def show
+  end
+
+private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :abstract, :content)
+  end
+end
