46ca386e70cc9fafecfd2e594da4d6b3c8c4ccb1
Anna Isabel Lamers <isabel.lamers@gmail.com>
2016-04-24 17:51:07 +0200

    Add admin to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150817114707) do
+ActiveRecord::Schema.define(version: 20160424151959) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "admins", ["user_id"], name: "index_admins_on_user_id", using: :btree
 
   create_table "comments", force: :cascade do |t|
     t.string   "name"
@@ -52,5 +60,6 @@ ActiveRecord::Schema.define(version: 20150817114707) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "admins", "users"
   add_foreign_key "comments", "posts"
 end
