653cb3451c7dc8b3ffefe287064d22d870187767
Anthony Limere <anthonylimere@gmail.com>
2016-09-05 15:54:15 +0200

    Add owl-carousel

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require fullpage
+//= require owl.carousel
 //= require bootstrap-sprockets
 //= require jquery-fileupload/basic
 //= require cloudinary/jquery.cloudinary
