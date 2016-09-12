3fa3714b17ecbcc232e75a2cb59be7ab8be80b91
Anna Isabel Lamers <isabel.lamers@gmail.com>
2016-09-12 12:39:40 +0800

    attempting to render html templates via js.erb

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,22 +1,25 @@
 class ContactsController < ApplicationController
   def new
     @contact = Contact.new
+    respond_to do |format|
+      format.html { render :layout => false }
+      format.js { render :action => 'new' }
+    end
   end
 
   def create
     @contact = Contact.new(params[:contact])
     @contact.request = request
+
+    respond_to do |format|
     if @contact.save
       @contact.deliver
-      render do |page|
-        page.html {}
-        page.js {}
-      end
+      format.html { redirect_to places_path, :notice => "Successfully created place" }
+      format.js
     else
-      flash.now[:error] = "You're message could not be sent."
-      render do |page|
-        page.html {}
-        page.js {}
+      flash.message[:error] = "You're message could not be sent."
+      format.html { render :action => 'new' }
+      format.js { render :action => 'new' }
       end
     end
 
