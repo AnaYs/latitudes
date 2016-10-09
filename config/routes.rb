41e5fbc7aa6a3893d20d660fc3a5dac1650c5e3c
Anna Isabel Lamers <isabel.lamers@gmail.com>
2016-10-09 13:24:14 +0800

    mailer config and routing

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   mount Attachinary::Engine => "/attachinary"
 
   get "/about", to: "pages#about"
-  resources :contacts, only: [:new, :create]
+  resources "contacts", only: [:new, :create]
 
   devise_for :users
   root "posts#index"
