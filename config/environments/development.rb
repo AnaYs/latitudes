e20f0ec818812b4e31a273e7285cef3938e8eed3
Anna Isabel Lamers <isabel.lamers@gmail.com>
2016-11-05 12:08:28 +0800

    Add setup for sendgrid smtp provider

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,13 +20,13 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
+  address: "smtp.sendgrid.net",
   port: "587",
-  domain: ENV["GMAIL_DOMAIN"],
+  domain: ENV["SENDGRID_DOMAIN"],
   authentication: "plain",
   enable_starttls_auto: true,
-  user_name: ENV["GMAIL_USERNAME"],
-  password: ENV["GMAIL_PASSWORD"]
+  user_name: ENV["SENDGRID_USERNAME"],
+  password: ENV["SENDGRID_PASSWORD"]
   }
 
   # Print deprecation notices to the Rails logger.
